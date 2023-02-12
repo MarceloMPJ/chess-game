7f4cca262bf0af499f9b5dfc74636d8a2614760f
Marcelo Junior <marcelo.jr63@gmail.com>
2023-02-12 19:04:57 -0300

    Move method private isFreePath of board to board/move

--- pkg/board/move.go
+++ pkg/board/move.go
@@ -1,6 +1,7 @@
 package board
 
 import (
+	"github.com/MarceloMPJ/chess-game/libs/basic"
 	"github.com/MarceloMPJ/chess-game/libs/values"
 	"github.com/MarceloMPJ/chess-game/pkg/piece"
 )
@@ -31,6 +32,43 @@ func (b *Board) nextTurn() {
 
 func (b *Board) allowMove(origin, dest values.Coord, p piece.PieceContract) bool {
 	return b.isCorrectTurn(origin) && p.IsValidMove(origin, dest) && b.isFreePath(origin, dest, p)
+}
+
+func (b *Board) isFreePath(origin, dest values.Coord, p piece.PieceContract) bool {
+	if b.isKnight(origin) {
+		return true
+	}
+
+	// When the path is horizontal or vertical
+	if origin.X == dest.X || origin.Y == dest.Y {
+		startX, startY := basic.MinUint8(origin.X, dest.X), basic.MinUint8(origin.Y, dest.Y)
+		finishX, finishY := basic.MaxUint8(origin.X, dest.X), basic.MaxUint8(origin.Y, dest.Y)
+
+		for i := startY; i <= finishY; i++ {
+			for j := startX; j <= finishX; j++ {
+				if origin.Y == i && origin.X == j {
+					continue
+				}
+
+				if b.hasPiece(i, j) {
+					return false
+				}
+			}
+		}
+	} else {
+		// When the path is horizontal
+		for i, j := origin.Y, origin.X; i != dest.Y && j != dest.X; i, j = nextStep(i, j, dest) {
+			if origin.Y == i && origin.X == j {
+				continue
+			}
+
+			if b.hasPiece(i, j) {
+				return false
+			}
+		}
+	}
+
+	return true
 }
 
 func (b *Board) isCorrectTurn(origin values.Coord) bool {
